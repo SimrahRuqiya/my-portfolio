200d193147a92523bf9cf99ae2a4aee1cd30bdbe
SimrahRuqiya <123015484+SimrahRuqiya@users.noreply.github.com>
2025-05-24 18:49:26 +0400

    added data

---
+++ src/data/portfolio.ts
@@ -0,0 +1,35 @@
+export const portfolioData = {
+    personal: {
+        name: "Simrah Shabandri",
+        title: "Computing & IT Student",
+        email: "simrahpatel16@gmail.com",
+        location: "Dubai, UAE",
+        github: "https://github.com/SimrahRuqiya",
+        linkedin: "https://www.linkedin.com/in/simrah-shabandri/",
+        bio: "I'm a passionate developer with a strong interest in IT, Networking, Cloud Computing, Dev Ops, Software Development and UI/UX Design. I love creating innovative solutions and exploring new technologies.",
+        cv: "src/assets/SimrahShabandri_CV.pdf"
+    },
+    projects: {
+        webDevelopement: [
+        {
+            id: 1,
+            title: "L2S Car Rental System",
+            description: "A comprehensive car rental system enabling users to browse, filter, and rent vehicles based on preferences, with dynamic car availability and detailed supplier information and built a review and feedback module allowing users to share rental experiences",
+            technologies: ["Java", "Spring Boot", "Semantic UI", "Mustache", "HTML", "CSS", "MariaDB"],
+            github: "https://github.com/SimrahRuqiya/project-l2s",
+            image: "src/assets/l2s.png",
+            date: "March 2025 - May 2025",
+            featured: true
+        },
+        {
+            id: 2,
+            title: "SpacePulse: Space Platform",
+            description: "A space website that provides information about space, including planets, stars, and galaxies. It features a user-friendly interface with interactive elements and responsive design.",
+            technologies: ["HTML5", "CSS"],
+            github: "https://github.com/SimrahRuqiya/space-pulse",
+            image: "src/assets/spacepulse.png",
+            date: "November 2024",
+            featured: true}
+    ]
+    }
+    }      
